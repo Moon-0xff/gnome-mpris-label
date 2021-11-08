730a234539995771115676a5d71e82236712db11
Moon-0xff <moon-0xff@protonmail.com>
2021-11-07 23:19:40 -0300

    Add prefs.js

---
+++ prefs.js
@@ -0,0 +1,198 @@
+'use strict';
+
+const {Gio,Gtk} = imports.gi;
+const ExtensionUtils = imports.misc.extensionUtils;
+
+const Config = imports.misc.config;
+const [major] = Config.PACKAGE_VERSION.split('.');
+const shellVersion = Number.parseInt(major);
+
+function init() {
+}
+
+function buildPrefsWidget() {
+    let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
+
+    let prefsWidget;
+    if (shellVersion < 40) {
+        prefsWidget = new Gtk.Grid({
+            margin: 18,
+            column_spacing: 12,
+            row_spacing: 12,
+            visible: true,
+            column_homogeneous: true,
+        });
+    } else {
+        prefsWidget = new Gtk.Grid({
+            margin_top: 10,
+            margin_bottom: 10,
+            margin_start: 10,
+            margin_end: 10,
+            column_spacing: 12,
+            row_spacing: 12,
+            visible: true,
+            column_homogeneous: true,
+        });
+    }
+
+    let title = new Gtk.Label({
+        label: '<b> Mpris Label Extension Preferences</b>',
+        halign: Gtk.Align.START,
+        use_markup: true,
+        visible: true
+    });
+    prefsWidget.attach(title, 0, 0, 1, 1);
+
+    let leftPaddingLabel = new Gtk.Label({
+        label: 'Left padding:',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(leftPaddingLabel, 0, 1, 1, 1);
+
+    let leftPaddingEntry = new Gtk.SpinButton({
+        adjustment: new Gtk.Adjustment({
+            lower: 0,
+            upper: 500,
+            step_increment: 1
+        }),
+        visible: true
+    });
+    prefsWidget.attach(leftPaddingEntry, 1, 1, 1, 1);
+
+    let rightPaddingLabel = new Gtk.Label({
+        label: 'Right padding:',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(rightPaddingLabel, 0, 2, 1, 1);
+
+    let rightPaddingEntry = new Gtk.SpinButton({
+        adjustment: new Gtk.Adjustment({
+            lower: 0,
+            upper: 500,
+            step_increment: 1
+        }),
+        visible: true
+    });
+    prefsWidget.attach(rightPaddingEntry, 1, 2, 1, 1);
+
+    let maxStringLengthLabel = new Gtk.Label({
+        label: 'Max string length (Each field):',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(maxStringLengthLabel, 0, 3, 1, 1);
+
+    let maxStringLengthEntry = new Gtk.SpinButton({
+        adjustment: new Gtk.Adjustment({
+            lower: 1,
+            upper: 150,
+            step_increment: 1
+        }),
+        visible: true
+    });
+    prefsWidget.attach(maxStringLengthEntry, 1, 3, 1, 1);
+
+    let extensionIndexLabel = new Gtk.Label({
+        label: 'Extension index:',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(extensionIndexLabel, 0, 4, 1, 1);
+
+    let extensionIndexEntry = new Gtk.SpinButton({
+        adjustment: new Gtk.Adjustment({
+            lower: 0,
+            upper: 20,
+            step_increment: 1
+        }),
+        visible: true
+    });
+    prefsWidget.attach(extensionIndexEntry, 1, 4, 1, 1);
+
+    let extensionPlaceLabel = new Gtk.Label({
+        label: 'Extension place:',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(extensionPlaceLabel, 0, 5, 1, 1);
+
+    let extensionPlaceComboBox = new Gtk.ComboBoxText({
+        halign: Gtk.Align.END,
+        visible: true
+    });
+    let options = ['left','center','right']
+    options.forEach(element => {
+        extensionPlaceComboBox.append(element,element);
+    });
+    extensionPlaceComboBox.set_active(options.indexOf(settings.get_string('extension-place')));
+    prefsWidget.attach(extensionPlaceComboBox, 1, 5, 1, 1);
+
+    let refreshRateLabel = new Gtk.Label({
+        label: 'Refresh rate (milliseconds):',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(refreshRateLabel, 0, 6, 1, 1);
+
+    let refreshRateEntry = new Gtk.SpinButton({
+        adjustment: new Gtk.Adjustment({
+            lower: 30,
+            upper: 3000,
+            step_increment: 1
+        }),
+        visible: true
+    });
+    prefsWidget.attach(refreshRateEntry, 1, 6, 1, 1);
+
+    let buttonPlaceHolderLabel = new Gtk.Label({
+        label: "Button place holder (can be left empty):",
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(buttonPlaceHolderLabel, 0, 7, 1, 1);
+
+    let buttonPlaceHolderEntry = new Gtk.Entry({
+        visible: true
+    });
+    prefsWidget.attach(buttonPlaceHolderEntry, 1, 7, 1, 1);
+
+    let removeRemasterTextLabel = new Gtk.Label({
+        label: 'Remove remaster text:',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(removeRemasterTextLabel, 0, 8, 1, 1);
+
+    let removeRemasterTextSwitch = new Gtk.Switch({
+    	valign: Gtk.Align.END,
+    	halign: Gtk.Align.END,
+    	visible: true
+    });
+    prefsWidget.attach(removeRemasterTextSwitch, 1, 8, 1, 1);
+
+    let dividerStringLabel = new Gtk.Label({
+        label: 'Divider String (you can use spaces):',
+        halign: Gtk.Align.START,
+        visible: true
+    });
+    prefsWidget.attach(dividerStringLabel, 0, 9, 1, 1);
+
+    let dividerStringEntry = new Gtk.Entry({
+        visible: true
+    });
+    prefsWidget.attach(dividerStringEntry, 1, 9, 1, 1);
+
+    settings.bind('left-padding',leftPaddingEntry,'value',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('right-padding',rightPaddingEntry,'value',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('max-string-length',maxStringLengthEntry,'value',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('extension-index',extensionIndexEntry,'value',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('extension-place',extensionPlaceComboBox,'active',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('refresh-rate',refreshRateEntry,'value',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('button-placeholder',buttonPlaceHolderEntry,'text',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('remove-remaster-text',removeRemasterTextSwitch,'active',Gio.SettingsBindFlags.DEFAULT);
+    settings.bind('divider-string',dividerStringEntry,'text',Gio.SettingsBindFlags.DEFAULT);
+
+    return prefsWidget;
+}
