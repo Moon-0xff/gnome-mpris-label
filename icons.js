7f134f030c329d37a0c69249ae0d51cd4c57b625
Batwam <batwam0@gmail.com>
2022-11-26 13:36:15 +0800

    added icon fallback for flatpak

--- icons.js
+++ icons.js
@@ -5,6 +5,7 @@ var IconHandler = class IconHandler {
 		return new St.Icon({
 			icon_name: app_name,
 			style_class: 'system-status-icon',
+			fallback_icon_name: "com."+app_name+".Client", // icon name for flatpak, in case it's not a native build
 			y_align: Clutter.ActorAlign.CENTER
 		});
 	}
