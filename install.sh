79dccc325419bc3827353e974f5d76f9d3294da5
Batwam <56710655+Batwam@users.noreply.github.com>
2022-12-02 19:20:10 +0800

    failsafe for install.sh

--- install.sh
+++ install.sh
@@ -6,5 +6,10 @@ if [ -d $DEFAULT_INSTALL_DIR ]; then
 fi
 
 mkdir -p $DEFAULT_INSTALL_DIR
+cd "$(dirname "$0")"
+printf "\e[32mCopying extension files to target directory:\n\e[0m"
 cp -Rv ./* $DEFAULT_INSTALL_DIR
-echo Done
+printf "\n\e[32mAll files copied. Please reload by logging back in if using Wayland or reload the shell (Alt + F2, r) if using X11\n\n\e[0m"
+
+#keep terminal open if script run from file browser
+$SHELL
