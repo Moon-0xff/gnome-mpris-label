877eaa8ddd03df060bacb5b1494165afb16b2bdd
Moon-0xff <moon-0xff@protonmail.com>
2023-01-16 19:20:25 -0300

    Add more changes requested by extensions.gnome.org
    - Remove prefsWidget and settings from global scope
    - Attach settings to prefsWidget
    - Pass prefsWidget as argument in prefs.js functions

--- prefs.js
+++ prefs.js
@@ -3,8 +3,6 @@ const {Gio,Gtk} = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Config = imports.misc.config;
 
-let prefsWidget;
-let settings;
 let position = 0;
 
 function init(){}
@@ -13,7 +11,7 @@ function buildPrefsWidget(){
 	const [major] = Config.PACKAGE_VERSION.split('.');
 	const shellVersion = Number.parseInt(major);
 
-	settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
+	let prefsWidget;
 
 	if(shellVersion < 40){
 		prefsWidget = new Gtk.Grid({
@@ -37,18 +35,21 @@ function buildPrefsWidget(){
 		});
 	}
 
-	addSpinButton('left-padding','Left padding:',0,500);
-	addSpinButton('right-padding','Right padding:',0,500);
-	addSpinButton('max-string-length','Max string length (each field):',1,150);
-	addSpinButton('extension-index','Extension index:',0,20);
-	let extensionPlaceComboBox = addStringComboBox('extension-place','Extension place:',{'left':'left','center':'center','right':'right'});
-	addSpinButton('refresh-rate','Refresh rate:',30,3000);
-	addEntry('button-placeholder','Button placeholder (can be left empty):');
-	addSwitch('remove-remaster-text','Remove remaster text:');
-	addEntry('divider-string','Divider string (you can use spaces):');
+	let settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.mpris-label');
+	prefsWidget._settings = settings;
+
+	addSpinButton(prefsWidget,'left-padding','Left padding:',0,500);
+	addSpinButton(prefsWidget,'right-padding','Right padding:',0,500);
+	addSpinButton(prefsWidget,'max-string-length','Max string length (each field):',1,150);
+	addSpinButton(prefsWidget,'extension-index','Extension index:',0,20);
+	let extensionPlaceComboBox = addStringComboBox(prefsWidget,'extension-place','Extension place:',{'left':'left','center':'center','right':'right'});
+	addSpinButton(prefsWidget,'refresh-rate','Refresh rate:',30,3000);
+	addEntry(prefsWidget,'button-placeholder','Button placeholder (can be left empty):');
+	addSwitch(prefsWidget,'remove-remaster-text','Remove remaster text:');
+	addEntry(prefsWidget,'divider-string','Divider string (you can use spaces):');
 
 	//visible fields is a bit more complex
-	addLabel('Visible fields and order:');
+	addLabel(prefsWidget,'Visible fields and order:');
 
 	let visibleFieldsBox = new Gtk.Box({
 		spacing: 12,
@@ -57,12 +58,12 @@ function buildPrefsWidget(){
 
 	let fieldOptions = {'artist':'xesam:artist','album':'xesam:album','title':'xesam:title'};
 
-	let firstFieldComboBox = buildStringComboBox('first-field',fieldOptions);
+	let firstFieldComboBox = buildStringComboBox(settings,'first-field',fieldOptions);
 
 	fieldOptions['none'] = '';
 
-	let secondFieldComboBox = buildStringComboBox('second-field',fieldOptions);
-	let lastFieldComboBox = buildStringComboBox('last-field',fieldOptions);
+	let secondFieldComboBox = buildStringComboBox(settings,'second-field',fieldOptions);
+	let lastFieldComboBox = buildStringComboBox(settings,'last-field',fieldOptions);
 
 	if(shellVersion < 40){
 		visibleFieldsBox.pack_start(firstFieldComboBox,true,true,0);
@@ -77,10 +78,10 @@ function buildPrefsWidget(){
 	prefsWidget.attach(visibleFieldsBox,1,position,1,1);
 	position++;
 
-	addSwitch('remove-text-when-paused','Hide when paused:');
-	addSpinButton('remove-text-paused-delay','Hide when paused delay (seconds):',0,10800);
-	addSwitch('auto-switch-to-most-recent','Switch to the most recent source automatically:');
-	let showIconComboBox = addStringComboBox('show-icon','Show source icon:',{'off':'','left':'left','right':'right'});
+	addSwitch(prefsWidget,'remove-text-when-paused','Hide when paused:');
+	addSpinButton(prefsWidget,'remove-text-paused-delay','Hide when paused delay (seconds):',0,10800);
+	addSwitch(prefsWidget,'auto-switch-to-most-recent','Switch to the most recent source automatically:');
+	let showIconComboBox = addStringComboBox(prefsWidget,'show-icon','Show source icon:',{'off':'','left':'left','right':'right'});
 
 	let resetButton = new Gtk.Button({
 		label: 'Reset settings',
@@ -115,8 +116,8 @@ function buildPrefsWidget(){
 	return prefsWidget
 }
 
-function addSpinButton(setting,labelstring,lower,upper){
-	addLabel(labelstring);
+function addSpinButton(widget,setting,labelstring,lower,upper){
+	addLabel(widget,labelstring);
 	let thisSpinButton = new Gtk.SpinButton({
 		adjustment: new Gtk.Adjustment({
 			lower: lower,
@@ -125,52 +126,52 @@ function addSpinButton(setting,labelstring,lower,upper){
 		}),
 		visible: true
 	});
-	prefsWidget.attach(thisSpinButton,1,position,1,1);
-	settings.bind(setting,thisSpinButton,'value',Gio.SettingsBindFlags.DEFAULT);
+	widget.attach(thisSpinButton,1,position,1,1);
+	widget._settings.bind(setting,thisSpinButton,'value',Gio.SettingsBindFlags.DEFAULT);
 	position++;
 }
 
-function addStringComboBox(setting,labelstring,options){
-	addLabel(labelstring);
-	thisComboBox = buildStringComboBox(setting,options);
-	prefsWidget.attach(thisComboBox,1,position,1,1);
+function addStringComboBox(widget,setting,labelstring,options){
+	addLabel(widget,labelstring);
+	thisComboBox = buildStringComboBox(widget._settings,setting,options);
+	widget.attach(thisComboBox,1,position,1,1);
 	position++;
 
 	return thisComboBox //necessary to reset position when the reset button is clicked
 }
 
-function addSwitch(setting,labelstring){
-	addLabel(labelstring);
+function addSwitch(widget,setting,labelstring){
+	addLabel(widget,labelstring);
 	let thisSwitch = new Gtk.Switch({
 		valign: Gtk.Align.END,
 		halign: Gtk.Align.END,
 		visible: true
 	});
-	prefsWidget.attach(thisSwitch,1,position,1,1);
-	settings.bind(setting,thisSwitch,'active',Gio.SettingsBindFlags.DEFAULT);
+	widget.attach(thisSwitch,1,position,1,1);
+	widget._settings.bind(setting,thisSwitch,'active',Gio.SettingsBindFlags.DEFAULT);
 	position++;
 }
 
-function addEntry(setting,labelstring){
-	addLabel(labelstring);
+function addEntry(widget,setting,labelstring){
+	addLabel(widget,labelstring);
 	let thisEntry = new Gtk.Entry({
 		visible: true
 	});
-	prefsWidget.attach(thisEntry,1,position,1,1);
-	settings.bind(setting,thisEntry,'text',Gio.SettingsBindFlags.DEFAULT);
+	widget.attach(thisEntry,1,position,1,1);
+	widget._settings.bind(setting,thisEntry,'text',Gio.SettingsBindFlags.DEFAULT);
 	position++;
 }
 
-function addLabel(labelstring){
+function addLabel(widget,labelstring){
 	let thisLabel = new Gtk.Label({
 		label: labelstring,
 		halign: Gtk.Align.START,
 		visible: true
 	});
-	prefsWidget.attach(thisLabel,0,position,1,1);
+	widget.attach(thisLabel,0,position,1,1);
 }
 
-function buildStringComboBox(setting,options){
+function buildStringComboBox(settings,setting,options){
 	let thisComboBox = new Gtk.ComboBoxText({
 		halign: Gtk.Align.END,
 		visible: true
