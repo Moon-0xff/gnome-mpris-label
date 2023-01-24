a07e9740c500348fc58b01006347ccc3f447454d
Moon-0xff <moon-0xff@protonmail.com>
2023-01-24 12:53:44 -0300

    Micro optimization

--- players.js
+++ players.js
@@ -98,20 +98,22 @@ var Players = class Players {
 		this.list = this.list.filter(element => dBusList.includes(element.address));
 
 		const SOURCES_BLACKLIST = this.settings.get_string('mpris-sources-blacklist');
-		const SOURCES_WHITELIST = this.settings.get_string('mpris-sources-whitelist');
 		const USE_WHITELIST = this.settings.get_boolean('use-whitelisted-sources-only');
 
-		const blacklist = SOURCES_BLACKLIST.replaceAll(' ','').split(',');
-		const whitelist = SOURCES_WHITELIST.replaceAll(' ','').split(',');
+		if(SOURCES_BLACKLIST || USE_WHITELIST){
+			const SOURCES_WHITELIST = this.settings.get_string('mpris-sources-whitelist');
+			const blacklist = SOURCES_BLACKLIST.replaceAll(' ','').split(',');
+			const whitelist = SOURCES_WHITELIST.replaceAll(' ','').split(',');
 
-		this.list = this.list.filter(function(element){
-			let source_name = element.address.replace('org.mpris.MediaPlayer2.','');
-			source_name = source_name.replace(/\.instance.*/g,'');
-			if (blacklist.includes(source_name) || (whitelist.includes(source_name) && USE_WHITELIST))
-				return false
+			this.list = this.list.filter(function(element){
+				let source_name = element.address.replace('org.mpris.MediaPlayer2.','');
+				source_name = source_name.replace(/\.instance.*/g,'');
+				if (blacklist.includes(source_name) || (whitelist.includes(source_name) && USE_WHITELIST))
+					return false
 
-			return true
-		});
+				return true
+			});
+		}
 
 		let addresses = [];
 		this.list.forEach(element => {
