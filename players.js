ce455d1393be1426545fe819a57544bbf4fd21ca
Moon-0xff <moon-0xff@protonmail.com>
2023-06-09 06:12:37 -0400

    Remove 'ArtUrl' property from 'mprisInterface'

--- players.js
+++ players.js
@@ -15,7 +15,6 @@ const mprisInterface = `
 		<property name="CanGoPrevious" type="b" access="read" />
 		<property name="Metadata" type="a{sv}" access="read"/>
 		<property name="PlaybackStatus" type="s" access="read"/>
-		<property name="ArtUrl" type="s" access="read"/>
 	</interface>
 
 </node>`
