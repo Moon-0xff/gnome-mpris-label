dfa0b4c7cf68e9a38898300361e661920ed04adb
Moon-0xff <moon-0xff@protonmail.com>
2023-06-15 16:55:32 -0400

    Unbind 'changed::padding-icon' from '_setIcon'
    to prevent crashes, changes will take effect on the next refresh

--- extension.js
+++ extension.js
@@ -62,7 +62,6 @@ class MprisLabel extends PanelMenu.Button {
 		this.settings.connect('changed::show-icon',this._setIcon.bind(this));
 		this.settings.connect('changed::use-album',this._setIcon.bind(this));
 		this.settings.connect('changed::symbolic-source-icon', this._setIcon.bind(this));
-		this.settings.connect('changed::padding-icon', this._setIcon.bind(this));
 
 		Main.panel.addToStatusArea('Mpris Label',this,EXTENSION_INDEX,EXTENSION_PLACE);
 
