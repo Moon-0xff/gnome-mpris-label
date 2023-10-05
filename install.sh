9da6557e6ed904c9ad401b38b1301d3786a716e0
Batwam <56710655+Batwam@users.noreply.github.com>
2023-10-05 13:42:21 +0800

    Apply gnome45 patch automatically (#85)
    Co-authored-by: Moon-0xff <moon-0xff@protonmail.com>

--- install.sh
+++ install.sh
@@ -10,12 +10,16 @@ cd "$(dirname "$0")"
 printf "\e[32mCopying extension files to target directory:\n\e[0m"
 cp -Rv ./* $DEFAULT_INSTALL_DIR
 
+GNOME_VERSION=$(gnome-shell --version | awk -F'[ .]' '{print $3}') #get major Gnome version
+if [ $GNOME_VERSION -ge 45 ]; then #apply patch if Gnome 45+
+	echo "You are running GNOME 45 or above. The script will try to patch compatibility for this version. See README.md for details."
+	patch -d $DEFAULT_INSTALL_DIR < patches/gnome45-compatibility.patch && echo "Patch applied!" || echo "Patch failed!" && exit 1
+fi
+
 if [ $XDG_SESSION_TYPE = "x11" ]; then
 	printf "\n\e[32mAll files copied. \nPlease reload the gnome-shell (shortcut Alt + F2, r) to load the extension.\n\n\e[0m"
 else
 	printf "\n\e[32mAll files copied. \nPlease log out and log back in again to load the extension.\n\n\e[0m"
 fi
-
-printf "\e[33mIf you are running GNOME 45 you will need to apply the compatibility patch. See README.md for instructions.\n\n\e[0m"
 
 cd $OLDPWD
