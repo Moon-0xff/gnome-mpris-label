ce7946cfee749afd1c64193a97a5c5f03b39c299
Batwam <56710655+Batwam@users.noreply.github.com>
2024-06-04 22:13:28 +0800

    remove logs

--- extension.js
+++ extension.js
@@ -167,7 +167,6 @@ class MprisLabel extends PanelMenu.Button {
 
 	_onScroll(event) {
 		const SCROLL_ACTION = this.settings.get_string('scroll-action');
-		log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: ---------------------------------- ');
 		
 		if(SCROLL_ACTION == 'none')
 			return Clutter.EVENT_STOP
@@ -175,31 +174,26 @@ class MprisLabel extends PanelMenu.Button {
 		if (event.is_pointer_emulated())
 			return Clutter.EVENT_PROPAGATE;
 		
-		log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: get_scroll_direction - '+event.get_scroll_direction());
-
 		let delta = 0;
 		const time_delta = Date.now() - this.last_scroll;
 		switch (event.get_scroll_direction()) {
 			// UP=0,DOWN=1,LEFT=2,RIGHT=3,SMOOTH=4
 			case Clutter.ScrollDirection.SMOOTH:
 				delta = -event.get_scroll_delta()[1];
-				log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: Smooth	'+delta);
 				delta = Math.clamp(-1,delta,1);
 				this.last_scroll = new Date().getTime();
 				break;
 
 			case Clutter.ScrollDirection.UP:
-				if (!this.last_scroll || time_delta > 500) {
+				if (!this.last_scroll || time_delta > 500)
 					delta = 0.25;
-					log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: UP	'+delta);
-				}
+
 				break;
 		
 			case Clutter.ScrollDirection.DOWN:
-				if (!this.last_scroll || time_delta > 500) {
+				if (!this.last_scroll || time_delta > 500)
 					delta = -0.25;
-					log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: DOWN	'+delta);
-				}
+
 				break;
 
 			default: //exit (do nothing)
@@ -209,7 +203,6 @@ class MprisLabel extends PanelMenu.Button {
 		if(!delta == 0)
 			switch(SCROLL_ACTION) {
 				case "volume-controls":
-					log(Date().substring(16,24)+' gnome-mpris-label-batwam/extension.js: scroll	('+event.get_scroll_direction()+') '+delta);
 					this._changeVolume(delta);
 					break;
 				case "track-change":
