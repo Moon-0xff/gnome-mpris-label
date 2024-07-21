e7297bcd439d1409746ba3972d34255465941a25
Moon-0xff <moon-0xff@protonmail.com>
2024-07-21 00:35:51 -0400

    Don't package `screenshot.png` on ego releases

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script packages the extension for ego release
 
-zip -r extension.zip label.js metadata.json prefs.js schemas/ extension.js LICENSE players.js README.md screenshot.png
+zip -r extension.zip label.js metadata.json prefs.js schemas/ extension.js LICENSE players.js README.md
 echo
 unzip -l extension.zip
