a015fbb68f367504b019fab323950d7fe8df5a27
Batwam <56710655+Batwam@users.noreply.github.com>
2024-10-17 12:48:39 +0800

    Replace Clutter.Color with Cogl.Color for Gnome 47+ (#109)

--- install.sh
+++ install.sh
@@ -13,7 +13,11 @@ cp -Rv ./* $DEFAULT_INSTALL_DIR
 GNOME_VERSION=$(gnome-shell --version | awk -F'[ .]' '{print $3}') #get major Gnome version
 if [ $GNOME_VERSION -ge 45 ]; then #apply patch if Gnome 45+
 	echo "You are running GNOME 45 or above. The script will try to patch compatibility for this version. See README.md for details."
-	patch -d $DEFAULT_INSTALL_DIR < patches/gnome45-compatibility.patch && echo "Patch applied!" || echo "Patch failed!" && exit 1
+	patch -d $DEFAULT_INSTALL_DIR < patches/gnome45-compatibility.patch && echo "Gnome 45+ Patch applied!" || { echo "Patch failed!"; exit 1 ; }
+	if [ $GNOME_VERSION -ge 47 ]; then #apply patch if Gnome 47+
+		echo "You are running GNOME 47 or above."
+		patch -d $DEFAULT_INSTALL_DIR < patches/gnome47-compatibility.patch && echo "Gnome 47+ Patch applied!" || { echo "Patch failed!"; exit 1 ; }
+	fi
 fi
 
 if [ $XDG_SESSION_TYPE = "x11" ]; then
