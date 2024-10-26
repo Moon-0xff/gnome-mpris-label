79fba8a95ca21f1a0c2b2d51316225f3302269b0
Moon-0xff <moon-0xff@protonmail.com>
2024-10-26 20:04:44 -0300

    `package.sh`: Create zip file on $HOME/Downloads/

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script packages the extension for ego release
 
-zip -r extension.zip label.js metadata.json prefs.js schemas/ extension.js LICENSE players.js README.md
+zip -r $HOME/Downloads/extension.zip label.js metadata.json prefs.js schemas/ extension.js LICENSE players.js README.md
 echo
-unzip -l extension.zip
+unzip -l $HOME/Downloads/extension.zip
