8a6e2153e568af2e96cfe020f1316a176afceff1
Moon-0xff <moon-0xff@protonmail.com>
2024-10-26 21:14:35 -0300

    Add color codes to installation script messages

--- install.sh
+++ install.sh
@@ -7,16 +7,17 @@ fi
 
 mkdir -p $DEFAULT_INSTALL_DIR
 cd "$(dirname "$0")"
-printf "\e[32mCopying extension files to target directory:\n\e[0m"
-cp -Rv ./* $DEFAULT_INSTALL_DIR
+printf "\e[32mCopying extension files to target directory...\e[0m"
+cp -R ./* $DEFAULT_INSTALL_DIR
+printf "\e[32mDone!\e[0m\n"
 
 GNOME_VERSION=$(gnome-shell --version | awk -F'[ .]' '{print $3}') #get major Gnome version
 if [ $GNOME_VERSION -ge 45 ]; then #apply patch if Gnome 45+
-	echo "You are running GNOME 45 or above. The script will try to patch compatibility for this version. See README.md for details."
-	patch -d $DEFAULT_INSTALL_DIR < patches/gnome45-compatibility.patch && echo "Gnome 45+ Patch applied!" || { echo "Patch failed!"; exit 1 ; }
+	printf "\e[33mYou are running GNOME 45 or above. The script will try to patch compatibility for this version. See README.md for details.\e[0m\n"
+	patch -d $DEFAULT_INSTALL_DIR < patches/gnome45-compatibility.patch && printf "\e[32mGnome 45+ Patch applied!\e[0m\n" || { printf "\e[31mPatch failed!\e[0m\n"; exit 1 ; }
 	if [ $GNOME_VERSION -ge 47 ]; then #apply patch if Gnome 47+
-		echo "You are running GNOME 47 or above."
-		patch --no-backup-if-mismatch -d $DEFAULT_INSTALL_DIR < patches/gnome47-compatibility.patch && echo "Gnome 47+ Patch applied!" || { echo "Patch failed!"; exit 1 ; }
+		printf "\e[33mYou are running GNOME 47 or above.\e[0m\n"
+		patch --no-backup-if-mismatch -d $DEFAULT_INSTALL_DIR < patches/gnome47-compatibility.patch && printf "\e[32mGnome 47+ Patch applied!\e[0m\n" || { printf "\e[31mPatch failed!\e[0m\n"; exit 1 ; }
 	fi
 fi
 
